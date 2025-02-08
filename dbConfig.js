05a2b3f842004e160284c5e1d2bcd786b4829ebe
abhinavgit2002 <abhinavjnair61@gmail.com>
2025-02-08 11:29:30 +0530

    db config

--- dbConfig.js
+++ dbConfig.js
@@ -2,7 +2,7 @@ const sql = require('mssql/msnodesqlv8');
 
 const config = {
   //  connectionString: "Driver={ODBC Driver 18 for SQL Server};Server=LAPTOP-QAF46VQE;Database=HarvestHub;Trusted_Connection=yes;TrustServerCertificate=yes;"
-  connectionString: "Driver={ODBC Driver 18 for SQL Server};Server=mssql-191972-0.cloudclusters.net,10218;Database=HarvestHub;User ID=admin;Password=Adm!n001;TrustServerCertificate=yes;"
+  connectionString: "Driver={ODBC Driver 18 for SQL Server};Server=mssql-191972-0.cloudclusters.net,10218;Database=HarvestHub;UID=admin;PWD=Adm!n001;TrustServerCertificate=yes;"
 
 };
 
